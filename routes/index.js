8ea7017f0683e5c875e8b17f387037f985ac685f
Amanda Bausch <manadab@me.com>
2015-11-26 19:47:00 -0600

    resizing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var memes = require('../public/static/memes.json');
+var messages = require('../public/static/comments.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // renders the file named "index.jade" along with the data
+  res.render('index', {title: 'Buffy Meme', data: memes, comnt: messages});
 });
+
+// exports the router
 
 module.exports = router;
