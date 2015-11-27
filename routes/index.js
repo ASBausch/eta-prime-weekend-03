5f13c8a1d8a2b4c821dd7c3f28714875c42b163d
Amanda Bausch <manadab@me.com>
2015-11-26 23:18:25 -0600

    tied comment to meme in route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var memes = require('../public/static/memes.json');
-var messages = require('../public/static/comments.json');
+var comments = require('../public/static/comments.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // renders the file named "index.jade" along with the data
@@ -9,8 +9,34 @@ router.get('/', function(req, res, next) {
 });
 
 // creates a page /memes with images and comments
-router.get('/memes', function(req, res, next) {
-  res.render('meme', {title: 'Buffy Meme'});
+router.get('/memes/:id', function(req, res, next) {
+  //getting a request from memes with an id and
+  //we are setting that id to a variable
+  var memeID = req.params.id;
+
+  //we need to find out if the request origin id matches the meme id
+  var currentMeme;
+  for (i = 0; i < memes.length; i++) {
+    //if they match the currentMeme is set to that index
+    if (memes[i].id == memeID) {
+      currentMeme = memes[i];
+    }
+  }
+
+  var currentComment;
+  for (i = 0; i < comments.length; i++) {
+    if (comments[i].imageID == memeID) {
+      currentComment = comments[i];
+    }
+  }
+
+  //pass in the meme object with a specific index
+
+  //pass in the comment object with a specific matching index
+  //key value pairs - you reference the key in the template
+  res.render('meme', {title: 'Buffy Meme', meme: currentMeme,
+    comment: currentComment, });
+
 });
 
 // exports the router
